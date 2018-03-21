a922c3aa14480000f1d9612e6e9f24f4afe6eb57
Catherine Looper <loopercat@gmail.com>
2018-03-21 12:03:19 -0700

    adding get request to song-router

--- route/song-router.js
+++ route/song-router.js
@@ -2,18 +2,39 @@
 
 const {Router} = require('express');
 const jsonParser = require('body-parser').json();
-const httpError = require('http-errors');
+const httpErrors = require('http-errors');
 const Song = require('../model/song');
+const superagent = require('superagent');
 
 const songRouter = module.exports = new Router()
 ;
 
 //++++++++++++++++++GET+++++++++++++++++++++++
 
-songRouter.get('/lyrics', (request, response, next) => {
+songRouter.post('/lyrics', (request, response, next) => {
   if(!request.content) {
-    return next(new httpError(404, '__ERROR__ Not Found'));
+    return next(new httpErrors(404, '__ERROR__ Not Found'));
   }
 
-  return response.send(request.content);
+  return superagent.get(`http://www.kanyerest.xyz/api/album/graduation`)
+    .then(response => {
+      return response.body;
+    })
+    .then(songObject => {
+      return new Song ({
+        ...request.body,
+        content: songObject.lyrics,
+      }).save()
+        .then(song => response.json(song))
+        .catch(next);
+    })
+    .catch(next);
+});
+
+songRouter.get('/songs', (request, response, next) => {
+  if(!request.song)
+    return next(new httpErrors(404, '__ERROR__ Not Found'));
+  Song.findOne({content: request.lyrics})
+    .then(song => response.json(song))
+    .catch(next);
 });

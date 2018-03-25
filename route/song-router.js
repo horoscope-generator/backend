0af72a85762857c35609b741ef6778a0f0d1b53d
Catherine Looper <loopercat@gmail.com>
2018-03-24 17:11:26 -0700

    configuring routes

--- route/song-router.js
+++ route/song-router.js
@@ -1,26 +1,36 @@
 'use strict';
 
 const {Router} = require('express');
-const jsonParser = require('body-parser').json();
-const Song = require('../model/song');
+// const jsonParser = require('body-parser').json();
+// const Song = require('../model/song');
 const superagent = require('superagent');
+// const lyrics = require('song.json');
+let __API_URL__ = process.env.API_URL;
 
 const songRouter = module.exports = new Router();
 
 //++++++++++++++++++GET+++++++++++++++++++++++
 
-songRouter.post('/lyrics', jsonParser, (request, response, next) => {
+songRouter.get('/lyrics', (request, response, next) => {
 
   superagent.get(`http://www.kanyerest.xyz/api/album/graduation`)
-    .then(songObject => {
-      console.log('song object.result[0]', songObject.response.text.result[0]);
-      return new Song({
-        title: songObject.result[0].title,
-        lyrics: songObject.result[0].lyrics,
-      }).save();
-    })
-    .then(response => {
-      response.sendStatus(200);
-    })
-    .catch(next);
+    .then(songObject => response.send(songObject.text),
+      err => response.send(err)
+    );
 });
+
+// songRouter.get('/lyrics', jsonParser, (request, response, next) => {
+  
+//     superagent.get(`http://www.kanyerest.xyz/api/album/graduation`)
+//       .then(songObject => {
+//         console.log('song object.result[0]', songObject.response.text.result[0]);
+//         return new Song({
+//           title: songObject.result[0].title,
+//           lyrics: songObject.result[0].lyrics,
+//         }).save();
+//       })
+//       .then(response => {
+//         response.sendStatus(200);
+//       })
+//       .catch(next);
+//   });
